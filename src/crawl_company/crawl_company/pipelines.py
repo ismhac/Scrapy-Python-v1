a0220aa381c531fca769b3018957d3cce07bcbba
hienlehitek <hienle@hitek.com.vn>
2023-12-05 01:08:35 +0700

    update crawl and save to db

--- src/crawl_company/crawl_company/pipelines.py
+++ src/crawl_company/crawl_company/pipelines.py
@@ -3,63 +3,97 @@ from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 from scrapy.exceptions import DropItem
 from datetime import datetime
+from pymongo.errors import DuplicateKeyError
 
 class MongoPipeline(object):
-    company_schema = 'company_collection'
-    user_schema = 'users'
+    company_module = 'company_collection'
+    job_module = 'job_collection'
+    user_module = 'users'
 
     def open_spider(self, spider):
         load_dotenv(find_dotenv())
         mongo_uri:str = os.getenv("MONGO_URI")
         database_name:str = os.getenv("DATABASE_NAME")
 
-        # print(mongo_uri, database_name)
-
         self.client = MongoClient(mongo_uri)
         self.db = self.client[database_name]
 
         try:
             self.client.server_info() 
-            print("MongoDB connection successful")
+            print("Kết nối MongoDB thành công")
         except Exception as e:
-            print("Could not connect to MongoDB: ", e)
+            print("Không thể kết nối với MongoDB: ", e)
 
     def close_spider(self, spider):
         self.client.close()
 
     def process_item(self, item, spider):
         # Validation
-        if not item['name'] or not item['logo'] or not item['address'] or not item['link'] or not item['description']:
-            raise DropItem("Missing field in %s" % item)
+        if not item.get('company_name') or not item.get('company_logo') or not item.get('company_address') or not item.get('company_link') or not item.get('company_description'):
+            spider.logger.warning("Thiếu trường trong %s" % item)
+            return item
 
         # Get user information
-        user = self.db[self.user_schema].find_one({"email":"admin@gmail.com"})
+        user = self.db[self.user_module].find_one({"email": "admin@gmail.com"})
         if user:
-            item['createdBy'] = {'_id': str(user['_id']), 'email': user['email']}
+            item['company_createdBy'] = {'_id': str(user['_id']), 'email': user['email']}
         else:
-            raise DropItem("User not found")
+            spider.logger.error("Không tìm thấy người dùng cho %s" % item['company_name'])
+            return item
 
         now = datetime.now().isoformat()
-
-        if not self.db[self.company_schema].find_one({"name": item['name']}):
-            item['createdAt'] = now
-        item['updatedAt'] = now
 
         # Transaction
         with self.client.start_session() as session:
             session.start_transaction()
             try:
-                self.db[self.company_schema].update_one(
-                    {"name": item['name']}, 
-                    {"$set": dict(item)}, 
+                # Update or insert company
+                result = self.db[self.company_module].update_one(
+                    {"name": item['company_name'], "isDeleted": False},
+                    {"$set": {
+                        "name": item['company_name'],
+                        "address": item['company_address'],
+                        "description": item['company_description'],
+                        "logo": item['company_logo'],
+                        "createdBy": item['company_createdBy'],
+                        "createdAt": now,
+                        "updatedAt": now,
+                        "isDeleted": False,
+                        "deletedAt": None
+                        }
+                    },
                     upsert=True,
                     session=session
                 )
+
+                # Get the company_id
+                if result.upserted_id is not None:
+                    company_id = result.upserted_id
+                else:
+                    company = self.db[self.company_module].find_one({"name": item['company_name'], "isDeleted": False})
+                    company_id = company['_id']
+
+                # Store jobs information
+                for job in item.get('jobs', []):
+                    self.db[self.job_module].update_one(
+                        {"name": job['job_name'], "isDeleted": False},
+                        {"$set": {
+                            "name": job['job_name'],
+                            "skills": job['job_skills'],
+                            "company": {
+                                "_id": company_id,
+                                "name": item['company_name'],
+                                "logo": item['company_logo']
+                            }
+                        }},
+                        upsert=True,
+                        session=session
+                    )
+
                 session.commit_transaction()
+
             except Exception as e:
                 session.abort_transaction()
-                raise e
+                spider.logger.error("Lỗi khi xử lý mục: %s" % e)
 
         return item
-
-
--- src/crawl_company/crawl_company/spiders/spider_crawler.py
+++ src/crawl_company/crawl_company/spiders/spider_crawler.py
@@ -1,51 +1,91 @@
 from typing import Any, Iterable
 import scrapy
 from scrapy.http import Request, Response
-
+import json
+import re
 class Company(scrapy.Item):
-    link = scrapy.Field()
-    name = scrapy.Field()
-    logo = scrapy.Field()
-    address = scrapy.Field()
-    description = scrapy.Field()
-    createdBy  = scrapy.Field()
-    isDeleted = scrapy.Field()
-    createdAt = scrapy.Field()
-    updatedAt = scrapy.Field()
-    deletedAt = scrapy.Field()
-
+    company_link = scrapy.Field()
+    company_name = scrapy.Field()
+    company_logo = scrapy.Field()
+    company_address = scrapy.Field()
+    company_description = scrapy.Field()
+    company_createdBy  = scrapy.Field()
+    company_isDeleted = scrapy.Field()
+    company_createdAt = scrapy.Field()
+    company_updatedAt = scrapy.Field()
+    company_deletedAt = scrapy.Field()
+    jobs = scrapy.Field()
+class Job(scrapy.Item):
+    job_name = scrapy.Field()
+    job_link = scrapy.Field()
+    job_skills = scrapy.Field() 
+    job_link_id = scrapy.Field()
 class crawling(scrapy.Spider):
     name = "crawl_company"
+    domain = "https://topdev.vn"
     def start_requests(self) -> Iterable[Request]:
         url = "https://topdev.vn/nha-tuyen-dung?src=topdev.vn&medium=mainmenu.html"
         yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response: Response, **kwargs: Any) -> Any:
         links = response.xpath("//div[@id='post_data']//div[@class='frame style-2']/a[@class='link']//@href").extract()
         # print(links)
-        domain = "https://topdev.vn"
+        
         for url in links:
-            full_link = domain + url.strip()
+            full_link = self.domain + url.strip()
             yield scrapy.Request(url=full_link, callback=self.parse_company)
-    
     def parse_company(self, response: Response, **kwargs: Any) -> Any:
         items = Company()
-
+        items['jobs'] = []
+        # company
         name = response.xpath('//div[@id="common-information"]/div/div[2]/h1//text()').get()
-
         logo = response.xpath('//div[@id="common-information"]/div/div[1]/img//@src').get()
-
         address = response.xpath('//main[@id="company-detail-page"]/div[@class="container"]/div[@class="grid grid-cols-3 gap-6"]/div[@class="col-span-1"]/div[@class="py-6"]/div[@class="mt-4"]/div[@class="rounded bg-white"]/div[@class="p-4 pt-0"]//div[@class="mt-4"]/ul[@class="mt-2"]/li/div[@class="rounded border border-solid border-white p-2 transition-all"]/div[@class="flex items-start gap-2"]/p[@class="flex-1"]//text()').get()
-
         description = ''.join(response.xpath('//section[@id="company-profile"]/*').get())
-
-        # print(name)
-        # print(logo)
-        # print(address)
+        items['company_name'] = name
+        items['company_logo'] = logo
+        items['company_address'] = address
+        items['company_link'] = response.url
+        items['company_description'] = description
+        # jobs
+        job_links = response.xpath('//section[@id="opening-jobs"]/div[@class="mt-6"]/ul//li[@class="mb-4 last:mb-0"]/div[@class="rounded border border-solid border-gray-200 bg-white p-4 transition-all hover:shadow-sm"]/div[@class="flex items-start justify-between gap-2"]/div[@class="flex-1"]/h3[@class="line-clamp-1 text-sm font-bold lg:text-base"]/a[@class="transition-all hover:text-primary"]//@href').extract()
         
-        items['name'] = name
-        items['logo'] = logo
-        items['address'] = address
-        items['link'] = response.url
-        items['description'] = description
-        yield items
+        for url in job_links:
+            full_link = self.domain + url.strip()
+            request =  scrapy.Request(
+                url=full_link, 
+                callback=self.parse_job_link)
+            request.meta['company'] = items
+            yield request
+    def parse_job_link(self, response: Response, **kwargs: Any) -> Any:
+        items = Job()
+        link = response.url
+        id_company =  re.findall(r'\d+', link)
+        link_id = id_company[-1]
+        name = response.xpath('//section[@id="detailJobHeader"]/div[@class="w-3/4 flex flex-initial flex-col"]/h1[@class="text-2xl font-bold text-black"]//text()').get()
+        # Extract the script tag data
+        script_data = response.xpath('//script[@type="application/ld+json"][2]/text()').get()
+        # Parse the JSON data
+        # json_data = json.loads(script_data)
+        # json_data_2 = json.dumps(json_data)
+        # skills_string = json.loads(json_data_2).get('skills')
+        # skills = skills_string.split(', ') if skills_string else []
+
+        json_data = json.loads(script_data)
+        skills_string = json_data.get('skills')
+        skills = skills_string.split(', ') if skills_string else []
+
+
+        items['job_link'] = link
+        items['job_name'] = name
+        items['job_skills'] = skills
+        items['job_link_id'] = link_id
+
+        company = response.meta['company']
+        company['jobs'].append(items)
+
+        yield company
+
+
+
+        
+
